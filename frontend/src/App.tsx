f6c27033755ede49d698d4ed68f039d2ab2e6ccb
malik-ali-txend <malik.ali@txend.com>
2026-08-06 19:19:20 +0500

    added App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import { Routes, Route } from 'react-router-dom'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
-
+import LandingPage from './pages/LandingPage'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Landing page</div>} />
+      <Route path="/" element={<LandingPage/>} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
